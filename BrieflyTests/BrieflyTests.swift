8b979aeb0f18168d698c24a8756ca251626dae79
donnoel <78627833+donnoel@users.noreply.github.com>
2025-11-30 10:55:41 -0800

    chore: initial commit (Briefly project)

---
+++ BrieflyTests/BrieflyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrieflyTests.swift
+//  BrieflyTests
+//
+//  Created by Don Noel on 11/30/25.
+//
+
+import Testing
+@testable import Briefly
+
+struct BrieflyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
