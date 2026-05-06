5187543b20015f907509794b96dc9fc6aa0b5bdf
Don Noel <donnoel@icloud.com>
2026-05-06 16:23:26 -0700

    Deduplicate AI generation error classification via SurpriseMeErrorClassifier

--- Briefly/Views/AIGenerationSheet.swift
+++ Briefly/Views/AIGenerationSheet.swift
@@ -258,44 +258,6 @@ struct AIGenerationSheet: View {
     }
 
     private func errorClassification(for error: Error) -> String {
-        if let serviceError = error as? AIContentService.ServiceError {
-            switch serviceError {
-            case .emptyResponse:
-                return "empty_response"
-            case .invalidJSON:
-                return "decode_invalid_json"
-            case .dtoDecodingFailed:
-                return "decode_dto_failed"
-            case .validationFailed:
-                return "validation_failed"
-            case .jobTransportUnavailable:
-                return "job_transport_unavailable"
-            }
-        }
-
-        if let clientError = error as? BrieflyBackendClient.ClientError {
-            switch clientError {
-            case .badResponse:
-                return "backend_http_failure"
-            case .invalidResponse:
-                return "backend_invalid_envelope"
-            case .requestTimedOut:
-                return "backend_timeout"
-            case .transport:
-                return "backend_transport_failure"
-            case .jobNotFound:
-                return "job_not_found"
-            case .jobNotReady:
-                return "job_not_ready"
-            case .jobFailed:
-                return "job_failed"
-            }
-        }
-
-        if error is ContentRepository.RepositoryError {
-            return "persistence_failure"
-        }
-
-        return "unknown"
+        SurpriseMeErrorClassifier.classify(error)
     }
 }
